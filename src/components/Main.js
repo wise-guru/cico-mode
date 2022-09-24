b0c5355776d5d1f1f82b7059659a75a5a9a72db8
wise-guru <wise.guru123@gmail.com>
2022-09-24 17:03:36 -0400

    Add bmi calculator route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage";
+import BMICalculator from "./BMICalculator";
 
 function Main() {
   return (
     <Routes>
-      <Route />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/bmi-calculator" element={<BMICalculator />} />
     </Routes>
   );
 }
