9f064dace0536bbd9c4267f87430d059f2e11840
wise-guru <wise.guru123@gmail.com>
2022-09-24 17:02:32 -0400

    Add link to bmi calculator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/cicoCropped.png";
+
 function Header() {
   return (
-    <div>
-      <div>Header</div>
-    </div>
+    <header className="header">
+      <div>
+        <Link to={"/"}>
+          <div className="logo">
+            <img src={Logo} alt="Grey letter C" />
+          </div>
+        </Link>
+        <div className="title">CICO Mode</div>
+      </div>
+
+      <Link to={"/bmi-calculator"}>BMI Calculator</Link>
+    </header>
   );
 }
 
