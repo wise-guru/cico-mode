54d834d62a39e4617f698faafd5a526c46607b2c
wise-guru <wise.guru123@gmail.com>
2022-10-07 15:14:31 -0400

    Add condition for time

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,7 +1,7 @@
 import { tab } from "@testing-library/user-event/dist/tab";
 import { initializeApp } from "firebase/app";
 import { collection, doc, getDocs, getFirestore, updateDoc } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABOssmOXbt97PKwC1zEnnoZbfNgWW0Flo",
@@ -16,159 +16,71 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-function TableRow() {
+function TableRow(props) {
+  const { tableRows, weeks } = props;
   const storedStats = collection(db, "weight-calculator");
-  const [age, setAge] = useState("");
-  const [weight, setWeight] = useState("");
-  const [calories, setCalories] = useState("");
-  const [activity, setActivity] = useState("");
-  const [gender, setGender] = useState("");
-  const [height, setHeight] = useState("");
-  const [imperialModeOn, setImperialModeOn] = useState(true);
-  const [tdeeMultiplier, setTdeeMultiplier] = useState("");
-  const [bmr, setBmr] = useState("");
-  const [tableBmr, setTableBmr] = useState(0);
+  // const tableStats = doc(db, "weight-table", "table");
+  const storedTable = collection(db, "weight-table");
 
-  const today = new Date();
-  const weekFromToday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
-  console.log(weekFromToday);
+  //   const [tableRows, setTableRows] = useState();
+  const [addTableRows, setAddTableRows] = useState(true);
+  const [rows, setRows] = useState([]);
 
-  const twoWeeksFromToday = new Date(
-    weekFromToday.getFullYear(),
-    weekFromToday.getMonth(),
-    weekFromToday.getDate() + 7
-  );
-  console.log(`Two weeks: ${twoWeeksFromToday}`);
-
-  const monthFromToday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 60);
-  console.log(`Month from today: ${monthFromToday}`);
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "aa7f620368mshcec54851d525b6fp11dff9jsn36ee04ed8231",
-      "X-RapidAPI-Host": "bmr-and-tmr.p.rapidapi.com"
-    }
-  };
-
-  getDocs(storedStats).then((snapshot) => {
+  getDocs(storedTable).then((snapshot) => {
     snapshot.docs.forEach((document) => {
-      const personalStats = document.data().current[0];
-      console.log(personalStats.age);
+      const storedTableArray = document.data().current;
 
-      setGender(personalStats.gender);
-      setWeight(personalStats.weight);
-      setAge(personalStats.age);
-      setCalories(personalStats.calories);
-      setHeight(personalStats.height);
-      setActivity(personalStats.activity);
-      setImperialModeOn(personalStats.imperialMode);
-      setTdeeMultiplier(personalStats.tdeeMultiplier);
+      setRows(storedTableArray);
     });
-
-    if (imperialModeOn) {
-      const bmr =
-        gender === "female"
-          ? 4.536 * weight + 15.88 * height - 5 * age - 161
-          : 4.536 * weight + 15.88 * height - 5 * age + 5;
-      setBmr(bmr);
-      console.log(`${bmr} imperial`);
-    } else {
-      const bmr =
-        gender === "female"
-          ? 10 * weight + 6.25 * height - 5 * age - 161
-          : 10 * weight + 6.25 * height - 5 * age + 5;
-      setBmr(bmr);
-      console.log(`${bmr} metric`);
-    }
   });
 
-  const result = weekFromToday;
-  const date =
-    (result.getMonth().toString().length >= 1
-      ? result.getMonth() + 1
-      : "0" + (result.getMonth() + 1)) +
-    "/" +
-    (result.getDate().toString().length >= 1 ? result.getDate() : "0" + result.getDate()) +
-    "/" +
-    result.getFullYear();
-  //   console.log(date);
-  //   console.log(tdee);
-  //   console.log(activity);
-
-  const tdee = Math.round(bmr * tdeeMultiplier * 100) / 100;
-  const caloricDeficit = Math.round((tdee - calories) * 100) / 100;
-
-  const getTableRows = () => {
-    const tableRows = [
-      {
-        dayRow: date,
-        weightRow: weight,
-        caloriesBurnedRow: calories,
-        deficitRow: caloricDeficit
-      }
-    ];
-
-    for (let i = 0; i < 10; i++) {
-      const addedDay = (i + 1) * 7;
-      const date = new Date(
-        weekFromToday.getFullYear(),
-        weekFromToday.getMonth(),
-        weekFromToday.getDate() + addedDay
-      );
-
-      const formattedDate =
-        (date.getMonth().toString().length >= 1
-          ? date.getMonth() + 1
-          : "0" + (date.getMonth() + 1)) +
-        "/" +
-        (date.getDate().toString().length >= 1 ? date.getDate() : "0" + date.getDate()) +
-        "/" +
-        date.getFullYear();
-
-      const weightCalories = (tableRows[i].weightRow * 3500 - tableRows[i].deficitRow) / 3500;
-
-      if (imperialModeOn) {
-        const bmr =
-          gender === "female"
-            ? 4.536 * weightCalories + 15.88 * height - 5 * age - 161
-            : 4.536 * weightCalories + 15.88 * height - 5 * age + 5;
-        setTableBmr(bmr);
-        console.log(`${tableBmr} imperial`);
-      } else {
-        const bmr =
-          gender === "female"
-            ? 10 * weightCalories + 6.25 * height - 5 * age - 161
-            : 10 * weightCalories + 6.25 * height - 5 * age + 5;
-        setTableBmr(bmr);
-        console.log(`${tableBmr} metric`);
-      }
-      const tdee = Math.round(tableBmr * tdeeMultiplier * 100) / 100;
-      const caloricDeficit = Math.round((tdee - calories) * 100) / 100;
-
-      const num = {
-        dayRow: formattedDate,
-        weightRow: weightCalories,
-        caloriesBurnedRow: tdee,
-        deficitRow: caloricDeficit
-      };
-
-      tableRows.push(num);
-    }
-    console.log(tableRows);
-  };
-
-  console.log(tdee);
-  console.log(tdeeMultiplier);
-
   return (
-    <tr>
-      <th className="date">{date}</th>
-      <td className="weight">{weight}</td>
-      <td className="tdee">{tdee}</td>
-      <td className="deficit">{caloricDeficit}</td>
-      <a onClick={() => getTableRows()}>Table Rows</a>
-    </tr>
+    <div>
+      {/* <button
+        onClick={() => {
+          setAddTableRows(true);
+        }}>
+        Generate Table
+      </button> */}
+      <table className="weightTable">
+        <thead>
+          <tr>
+            <th scope="col">day</th>
+            <th scope="col">Weight</th>
+            <th scope="col">Calories Burned</th>
+            <th scope="col">Your Calorie Deficit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows !== [] || undefined ? (
+            rows.slice(0, weeks).map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td>{row.dayRow}</td>
+                  <td>{row.weightRow}</td>
+                  <td>{row.caloriesBurnedRow}</td>
+                  <td>{row.deficitRow}</td>
+                </tr>
+              );
+            })
+          ) : tableRows !== [] || undefined ? (
+            tableRows.slice(0, weeks).map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td>{row.dayRow}</td>
+                  <td>{row.weightRow}</td>
+                  <td>{row.caloriesBurnedRow}</td>
+                  <td>{row.deficitRow}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <div>404</div>
+          )}
+          {console.log(tableRows)}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
